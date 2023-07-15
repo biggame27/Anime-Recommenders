29815cd9898018738be846c610c12ebc02693ad5
Harrison <koharrisonko@gmail.com>
2023-07-15 02:03:59 -0500

    added an image

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ import anime_recommendations as ar
 #     return datar
 
 def show_page():
-    st.title("Anime Recommender Prediction")
+    st.title("Anime Recommender")
+    st.markdown("![Alt Text](https://wallpaperaccess.com/thumb/17350.jpg)")
     st.write("Note: There isn't a search engine so the anime has to exactly match the dataset:(.")
     st.write("Here are some recommendations:")
     arr = ['Gintama', 'Tokyo Ghoul', 'Fullmetal Alchemist: Brotherhood', 'Haikyuu!!', 'No Game No Life', 'Naruto', 'Sword Art Online', 'Shingeki no Kyojin']
